87777343cb741ca6a7394d9fac036bf7365dd8e0
Luke Horvat <lukehorvat@gmail.com>
2024-03-08 19:32:31 +0100

    Expand actors_defs.xml

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
+import * as THREE from 'three';
+import { XmlEntitiesExpander } from './io/xml-entities';
 import './index.css';
 
-console.log('yeah');
+void main();
+
+async function main(): Promise<void> {
+  const appEl = document.querySelector('.app')!;
+  const fileLoader = new THREE.FileLoader();
+  const xml = (await fileLoader.loadAsync(
+    'data/actor_defs/actor_defs.xml'
+  )) as string;
+  const xmlExpander = new XmlEntitiesExpander(xml);
+  const entityXmls: string[] = [];
+  for (const entityUri of xmlExpander.entityUris) {
+    const entityXml = (await fileLoader.loadAsync(
+      `data/actor_defs/${entityUri}`
+    )) as string;
+    entityXmls.push(entityXml);
+  }
+  const expandedXml = xmlExpander.expand(entityXmls);
+  appEl.textContent = expandedXml;
+}
