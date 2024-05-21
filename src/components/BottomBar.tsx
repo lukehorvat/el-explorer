838471051a3f5a104c1eccdcf02e23290c7f3bc8
Luke Horvat <lukehorvat@gmail.com>
2024-05-21 19:33:14 +0200

    Add dropdown for animation selection

--- src/components/BottomBar.tsx
+++ src/components/BottomBar.tsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import { useAtom, useAtomValue } from 'jotai';
+import { atoms } from '../lib/atoms';
+import { assetCache } from '../lib/asset-cache';
 import './BottomBar.css';
 
 export function BottomBar(): React.JSX.Element {
+  const isLoaded = useAtomValue(atoms.isLoaded);
+  const actorType = useAtomValue(atoms.actorType);
+  const actorDef = assetCache.actorDefs.get(actorType);
+  const [actorAnimation, setActorAnimation] = useAtom(atoms.actorAnimation);
+
   return (
     <div className="BottomBar">
-      <div>TODO</div>
+      {isLoaded && (
+        <div>
+          <label>Animation:</label>
+          <select
+            className="AnimationSelect"
+            value={actorAnimation ?? ''}
+            onChange={(event) => {
+              setActorAnimation(event.target.value || null);
+            }}
+          >
+            <option value="">None</option>
+            {actorDef!.animationFrames.map((frame) => (
+              <option value={frame.type} key={frame.type}>
+                {frame.type.replace(/^CAL_/, '')}
+              </option>
+            ))}
+          </select>
+        </div>
+      )}
     </div>
   );
 }
--- src/lib/atoms.ts
+++ src/lib/atoms.ts
@@ -2,8 +2,10 @@ import { atom } from 'jotai';
 
 const isLoaded = atom<boolean>(false);
 const actorType = atom<number>(77); // Initially default to Feros.
+const actorAnimation = atom<string | null>(null); // TODO: Reset animation when actorType changes?
 
 export const atoms = {
   isLoaded,
   actorType,
+  actorAnimation,
 };
