3878ce33123d22cf453aba4396a6353a407b0866
Luke Horvat <lukehorvat@gmail.com>
2024-05-21 18:58:39 +0200

    Add next and previous buttons

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -7,26 +7,59 @@ import './TopBar.css';
 export function TopBar(): React.JSX.Element {
   const isLoaded = useAtomValue(atoms.isLoaded);
   const [actorType, setActorType] = useAtom(atoms.actorType);
+  const sortedActorDefs = [...assetCache.actorDefs.values()].sort(
+    (def1, def2) => def1.name.localeCompare(def2.name)
+  );
+  const moveToActor = (direction: 'next' | 'previous'): void => {
+    const currentIndex = sortedActorDefs.findIndex(
+      (actorDef) => actorDef.type === actorType
+    );
+
+    let newIndex = currentIndex + (direction === 'next' ? 1 : -1);
+    if (newIndex < 0) {
+      newIndex = sortedActorDefs.length - 1;
+    } else if (newIndex >= sortedActorDefs.length) {
+      newIndex = 0;
+    }
+
+    setActorType(sortedActorDefs[newIndex].type);
+  };
 
   return (
     <div className="TopBar">
       <div className="Brand">Creatures of EL</div>
       {isLoaded && (
-        <select
-          className="ActorSelect"
-          value={actorType ?? undefined}
-          onChange={(event) => {
-            setActorType(Number(event.target.value));
-          }}
-        >
-          {[...assetCache.actorDefs.values()]
-            .sort((def1, def2) => def1.name.localeCompare(def2.name))
-            .map((actorDef) => (
+        <div>
+          <button
+            className="PreviousButton"
+            onClick={() => {
+              moveToActor('previous');
+            }}
+          >
+            Prev
+          </button>
+          <select
+            className="ActorSelect"
+            value={actorType}
+            onChange={(event) => {
+              setActorType(Number(event.target.value));
+            }}
+          >
+            {sortedActorDefs.map((actorDef) => (
               <option value={actorDef.type} key={actorDef.type}>
                 {actorDef.name}
               </option>
             ))}
-        </select>
+          </select>
+          <button
+            className="NextButton"
+            onClick={() => {
+              moveToActor('next');
+            }}
+          >
+            Next
+          </button>
+        </div>
       )}
       <div className="Fork">
         <a href="https://github.com/lukehorvat/el-creatures">GitHub</a>
--- src/lib/atoms.ts
+++ src/lib/atoms.ts
@@ -1,7 +1,7 @@
 import { atom } from 'jotai';
 
 const isLoaded = atom<boolean>(false);
-const actorType = atom<number | null>(null);
+const actorType = atom<number>(77); // Initially default to Feros.
 
 export const atoms = {
   isLoaded,
