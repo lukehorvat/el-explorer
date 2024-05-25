845dbb0a6ea138b794b255fd793a98bccf06db70
Luke Horvat <lukehorvat@gmail.com>
2024-05-25 23:54:12 +0200

    Group controls

--- src/components/LeftBar.tsx
+++ src/components/LeftBar.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { useAtom, useAtomValue } from 'jotai';
+import { useAtom } from 'jotai';
 import { atoms } from '../lib/state';
 import { assetCache } from '../lib/asset-cache';
 import './LeftBar.css';
 
 export function LeftBar(): React.JSX.Element {
-  const actorType = useAtomValue(atoms.actorType);
+  const [actorType, setActorType] = useAtom(atoms.actorType);
   const [skinType, setSkinType] = useAtom(atoms.skinType);
   const [showSkeleton, setShowSkeleton] = useAtom(atoms.showSkeleton);
   const [showGround, setShowGround] = useAtom(atoms.showGround);
@@ -14,92 +14,154 @@ export function LeftBar(): React.JSX.Element {
   const [animationType, setAnimationType] = useAtom(atoms.animationType);
   const [loopAnimation, setLoopAnimation] = useAtom(atoms.loopAnimation);
   const actorDef = assetCache.actorDefs.get(actorType)!;
+  const sortedActorDefs = [...assetCache.actorDefs.values()].sort(
+    (def1, def2) => def1.name.localeCompare(def2.name)
+  );
+  const moveToActor = (direction: 'next' | 'previous'): void => {
+    const currentIndex = sortedActorDefs.findIndex(
+      (actorDef) => actorDef.type === actorType
+    );
+
+    let newIndex = currentIndex + (direction === 'next' ? 1 : -1);
+    if (newIndex < 0) {
+      newIndex = sortedActorDefs.length - 1;
+    } else if (newIndex >= sortedActorDefs.length) {
+      newIndex = 0;
+    }
+
+    setActorType(sortedActorDefs[newIndex].type);
+  };
 
   return (
     <div className="LeftBar">
-      <div className="Control">
-        <label>Skin:</label>
-        <select
-          value={skinType ?? ''}
-          onChange={(event) => {
-            setSkinType((event.target.value || null) as typeof skinType);
-          }}
-        >
-          <option value="">None</option>
-          <option value="texture">Texture</option>
-          <option value="wireframe">Wireframe</option>
-          <option value="vectors">Vectors</option>
-          <option value="metal">Metal</option>
-          <option value="crystal">Crystal</option>
-          <option value="silhouette">Silhouette</option>
-        </select>
+      <div className="ControlGroup">
+        <div className="ControlGroupTitle">Creature</div>
+        <div className="Control">
+          <label>Type:</label>
+          <select
+            value={actorType}
+            onChange={(event) => {
+              setActorType(Number(event.target.value));
+            }}
+          >
+            {sortedActorDefs.map((actorDef) => (
+              <option value={actorDef.type} key={actorDef.type}>
+                {actorDef.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="Control MoveToActor">
+          <button
+            className="PreviousButton"
+            onClick={() => {
+              moveToActor('previous');
+            }}
+          >
+            Prev
+          </button>
+          <button
+            className="NextButton"
+            onClick={() => {
+              moveToActor('next');
+            }}
+          >
+            Next
+          </button>
+        </div>
       </div>
-      <div className="Control">
-        <label>Skeleton:</label>
-        <input
-          type="checkbox"
-          checked={showSkeleton}
-          onChange={(event) => {
-            setShowSkeleton(event.target.checked);
-          }}
-        />
+      <div className="ControlGroup">
+        <div className="ControlGroupTitle">Appearance</div>
+        <div className="Control">
+          <label>Skin:</label>
+          <select
+            value={skinType ?? ''}
+            onChange={(event) => {
+              setSkinType((event.target.value || null) as typeof skinType);
+            }}
+          >
+            <option value="">None</option>
+            <option value="texture">Texture</option>
+            <option value="wireframe">Wireframe</option>
+            <option value="vectors">Vectors</option>
+            <option value="metal">Metal</option>
+            <option value="crystal">Crystal</option>
+            <option value="silhouette">Silhouette</option>
+          </select>
+        </div>
+        <div className="Control">
+          <label>Skeleton:</label>
+          <input
+            type="checkbox"
+            checked={showSkeleton}
+            onChange={(event) => {
+              setShowSkeleton(event.target.checked);
+            }}
+          />
+        </div>
       </div>
-      <div className="Control">
-        <label>Animation:</label>
-        <select
-          value={animationType ?? ''}
-          onChange={(event) => {
-            setAnimationType(event.target.value || null);
-          }}
-        >
-          <option value="">None</option>
-          {actorDef.animationFrames.map((frame) => (
-            <option value={frame.type} key={frame.type}>
-              {frame.type.replace(/^CAL_/, '')}
-            </option>
-          ))}
-        </select>
+      <div className="ControlGroup">
+        <div className="ControlGroupTitle">Animation</div>
+        <div className="Control">
+          <label>Type:</label>
+          <select
+            value={animationType ?? ''}
+            onChange={(event) => {
+              setAnimationType(event.target.value || null);
+            }}
+          >
+            <option value="">None</option>
+            {actorDef.animationFrames.map((frame) => (
+              <option value={frame.type} key={frame.type}>
+                {frame.type.replace(/^CAL_/, '')}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="Control">
+          <label>Loop:</label>
+          <input
+            type="checkbox"
+            checked={loopAnimation}
+            onChange={(event) => {
+              setLoopAnimation(event.target.checked);
+            }}
+            disabled={!animationType}
+          />
+        </div>
       </div>
-      <div className="Control">
-        <label>Loop animation:</label>
-        <input
-          type="checkbox"
-          checked={loopAnimation}
-          onChange={(event) => {
-            setLoopAnimation(event.target.checked);
-          }}
-          disabled={!animationType}
-        />
-      </div>
-      <div className="Control">
-        <label>Ground:</label>
-        <input
-          type="checkbox"
-          checked={showGround}
-          onChange={(event) => {
-            setShowGround(event.target.checked);
-          }}
-        />
-      </div>
-      <div className="Control">
-        <label>Auto rotate:</label>
-        <input
-          type="checkbox"
-          checked={autoRotate}
-          onChange={(event) => {
-            setAutoRotate(event.target.checked);
-          }}
-        />
-      </div>
-      <div className="Control">
-        <label>Stats:</label>
-        <input
-          type="checkbox"
-          checked={showStats}
-          onChange={(event) => {
-            setShowStats(event.target.checked);
-          }}
-        />
+      <div className="ControlGroup">
+        <div className="ControlGroupTitle">Miscellaneous</div>
+        <div className="Control">
+          <label>Ground:</label>
+          <input
+            type="checkbox"
+            checked={showGround}
+            onChange={(event) => {
+              setShowGround(event.target.checked);
+            }}
+          />
+        </div>
+        <div className="Control">
+          <label>Auto rotate:</label>
+          <input
+            type="checkbox"
+            checked={autoRotate}
+            onChange={(event) => {
+              setAutoRotate(event.target.checked);
+            }}
+          />
+        </div>
+        <div className="Control">
+          <label>Stats:</label>
+          <input
+            type="checkbox"
+            checked={showStats}
+            onChange={(event) => {
+              setShowStats(event.target.checked);
+            }}
+          />
+        </div>
       </div>
     </div>
   );
--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,66 +1,10 @@
 import React from 'react';
-import { useAtom, useAtomValue } from 'jotai';
-import { atoms } from '../lib/state';
-import { assetCache } from '../lib/asset-cache';
 import './TopBar.css';
 
 export function TopBar(): React.JSX.Element {
-  const isLoaded = useAtomValue(atoms.isLoaded);
-  const [actorType, setActorType] = useAtom(atoms.actorType);
-  const sortedActorDefs = [...assetCache.actorDefs.values()].sort(
-    (def1, def2) => def1.name.localeCompare(def2.name)
-  );
-  const moveToActor = (direction: 'next' | 'previous'): void => {
-    const currentIndex = sortedActorDefs.findIndex(
-      (actorDef) => actorDef.type === actorType
-    );
-
-    let newIndex = currentIndex + (direction === 'next' ? 1 : -1);
-    if (newIndex < 0) {
-      newIndex = sortedActorDefs.length - 1;
-    } else if (newIndex >= sortedActorDefs.length) {
-      newIndex = 0;
-    }
-
-    setActorType(sortedActorDefs[newIndex].type);
-  };
-
   return (
     <div className="TopBar">
       <div className="Brand">Creatures of EL</div>
-      {isLoaded && (
-        <div>
-          <button
-            className="PreviousButton"
-            onClick={() => {
-              moveToActor('previous');
-            }}
-          >
-            Prev
-          </button>
-          <select
-            className="ActorSelect"
-            value={actorType}
-            onChange={(event) => {
-              setActorType(Number(event.target.value));
-            }}
-          >
-            {sortedActorDefs.map((actorDef) => (
-              <option value={actorDef.type} key={actorDef.type}>
-                {actorDef.name}
-              </option>
-            ))}
-          </select>
-          <button
-            className="NextButton"
-            onClick={() => {
-              moveToActor('next');
-            }}
-          >
-            Next
-          </button>
-        </div>
-      )}
       <div className="Fork">
         <a href="https://github.com/lukehorvat/el-creatures">GitHub</a>
       </div>
