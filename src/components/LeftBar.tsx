d767d7e340d060b6101897c5689b91dcb8c9c16d
Luke Horvat <lukehorvat@gmail.com>
2024-05-26 20:49:17 +0200

    Refactor left bar

--- src/components/LeftBar.tsx
+++ src/components/LeftBar.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
-import { useAtom } from 'jotai';
+import { useAtom, useAtomValue } from 'jotai';
 import { atoms } from '../lib/state';
 import { assetCache } from '../lib/asset-cache';
 import './LeftBar.css';
 
 export function LeftBar(): React.JSX.Element {
+  return (
+    <div className="LeftBar">
+      <CreatureControlGroup />
+      <AppearanceControlGroup />
+      <AnimationControlGroup />
+      <MiscControlGroup />
+    </div>
+  );
+}
+
+function CreatureControlGroup(): React.JSX.Element {
   const [actorType, setActorType] = useAtom(atoms.actorType);
-  const [skinType, setSkinType] = useAtom(atoms.skinType);
-  const [showSkeleton, setShowSkeleton] = useAtom(atoms.showSkeleton);
-  const [showGround, setShowGround] = useAtom(atoms.showGround);
-  const [showStats, setShowStats] = useAtom(atoms.showStats);
-  const [autoRotate, setAutoRotate] = useAtom(atoms.autoRotate);
-  const [animationType, setAnimationType] = useAtom(atoms.animationType);
-  const [loopAnimation, setLoopAnimation] = useAtom(atoms.loopAnimation);
-  const actorDef = assetCache.actorDefs.get(actorType)!;
   const sortedActorDefs = [...assetCache.actorDefs.values()].sort(
     (def1, def2) => def1.name.localeCompare(def2.name)
   );
@@ -33,136 +36,161 @@ export function LeftBar(): React.JSX.Element {
   };
 
   return (
-    <div className="LeftBar">
-      <div className="ControlGroup">
-        <div className="ControlGroupTitle">Creature</div>
-        <div className="Control">
-          <label>Type:</label>
-          <select
-            value={actorType}
-            onChange={(event) => {
-              setActorType(Number(event.target.value));
-            }}
-          >
-            {sortedActorDefs.map((actorDef) => (
-              <option value={actorDef.type} key={actorDef.type}>
-                {actorDef.name}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className="Control MoveToActor">
-          <button
-            className="PreviousButton"
-            onClick={() => {
-              moveToActor('previous');
-            }}
-          >
-            Prev
-          </button>
-          <button
-            className="NextButton"
-            onClick={() => {
-              moveToActor('next');
-            }}
-          >
-            Next
-          </button>
-        </div>
+    <div className="ControlGroup">
+      <div className="ControlGroupTitle">Creature</div>
+      <div className="Control">
+        <label>Type:</label>
+        <select
+          value={actorType}
+          onChange={(event) => {
+            setActorType(Number(event.target.value));
+          }}
+        >
+          {sortedActorDefs.map((actorDef) => (
+            <option value={actorDef.type} key={actorDef.type}>
+              {actorDef.name}
+            </option>
+          ))}
+        </select>
       </div>
-      <div className="ControlGroup">
-        <div className="ControlGroupTitle">Appearance</div>
-        <div className="Control">
-          <label>Skin:</label>
-          <select
-            value={skinType ?? ''}
-            onChange={(event) => {
-              setSkinType((event.target.value || null) as typeof skinType);
-            }}
-          >
-            <option value="">None</option>
-            <option value="texture">Texture</option>
-            <option value="wireframe">Wireframe</option>
-            <option value="vectors">Vectors</option>
-            <option value="metal">Metal</option>
-            <option value="crystal">Crystal</option>
-            <option value="silhouette">Silhouette</option>
-          </select>
-        </div>
-        <div className="Control">
-          <label>Skeleton:</label>
-          <input
-            type="checkbox"
-            checked={showSkeleton}
-            onChange={(event) => {
-              setShowSkeleton(event.target.checked);
-            }}
-          />
-        </div>
+      <div className="Control MoveToActor">
+        <button
+          className="PreviousButton"
+          onClick={() => {
+            moveToActor('previous');
+          }}
+        >
+          Prev
+        </button>
+        <button
+          className="NextButton"
+          onClick={() => {
+            moveToActor('next');
+          }}
+        >
+          Next
+        </button>
       </div>
-      <div className="ControlGroup">
-        <div className="ControlGroupTitle">Animation</div>
-        <div className="Control">
-          <label>Type:</label>
-          <select
-            value={animationType ?? ''}
-            onChange={(event) => {
-              setAnimationType(event.target.value || null);
-            }}
-          >
-            <option value="">None</option>
-            {actorDef.animationFrames.map((frame) => (
-              <option value={frame.type} key={frame.type}>
-                {frame.type.replace(/^CAL_/, '')}
-              </option>
-            ))}
-          </select>
-        </div>
-        {animationType && (
-          <div className="Control">
-            <label>Loop:</label>
-            <input
-              type="checkbox"
-              checked={loopAnimation}
-              onChange={(event) => {
-                setLoopAnimation(event.target.checked);
-              }}
-            />
-          </div>
-        )}
+    </div>
+  );
+}
+
+function AppearanceControlGroup(): React.JSX.Element {
+  const [skinType, setSkinType] = useAtom(atoms.skinType);
+  const [showSkeleton, setShowSkeleton] = useAtom(atoms.showSkeleton);
+
+  return (
+    <div className="ControlGroup">
+      <div className="ControlGroupTitle">Appearance</div>
+      <div className="Control">
+        <label>Skin:</label>
+        <select
+          value={skinType ?? ''}
+          onChange={(event) => {
+            setSkinType((event.target.value || null) as typeof skinType);
+          }}
+        >
+          <option value="">None</option>
+          <option value="texture">Texture</option>
+          <option value="wireframe">Wireframe</option>
+          <option value="vectors">Vectors</option>
+          <option value="metal">Metal</option>
+          <option value="crystal">Crystal</option>
+          <option value="silhouette">Silhouette</option>
+        </select>
       </div>
-      <div className="ControlGroup">
-        <div className="ControlGroupTitle">Miscellaneous</div>
+      <div className="Control">
+        <label>Skeleton:</label>
+        <input
+          type="checkbox"
+          checked={showSkeleton}
+          onChange={(event) => {
+            setShowSkeleton(event.target.checked);
+          }}
+        />
+      </div>
+    </div>
+  );
+}
+
+function AnimationControlGroup(): React.JSX.Element {
+  const actorType = useAtomValue(atoms.actorType);
+  const [animationType, setAnimationType] = useAtom(atoms.animationType);
+  const [loopAnimation, setLoopAnimation] = useAtom(atoms.loopAnimation);
+  const actorDef = assetCache.actorDefs.get(actorType)!;
+
+  return (
+    <div className="ControlGroup">
+      <div className="ControlGroupTitle">Animation</div>
+      <div className="Control">
+        <label>Type:</label>
+        <select
+          value={animationType ?? ''}
+          onChange={(event) => {
+            setAnimationType(event.target.value || null);
+          }}
+        >
+          <option value="">None</option>
+          {actorDef.animationFrames.map((frame) => (
+            <option value={frame.type} key={frame.type}>
+              {frame.type.replace(/^CAL_/, '')}
+            </option>
+          ))}
+        </select>
+      </div>
+      {animationType && (
         <div className="Control">
-          <label>Ground:</label>
+          <label>Loop:</label>
           <input
             type="checkbox"
-            checked={showGround}
+            checked={loopAnimation}
             onChange={(event) => {
-              setShowGround(event.target.checked);
+              setLoopAnimation(event.target.checked);
             }}
           />
         </div>
-        <div className="Control">
-          <label>Auto rotate:</label>
-          <input
-            type="checkbox"
-            checked={autoRotate}
-            onChange={(event) => {
-              setAutoRotate(event.target.checked);
-            }}
-          />
-        </div>
-        <div className="Control">
-          <label>Stats:</label>
-          <input
-            type="checkbox"
-            checked={showStats}
-            onChange={(event) => {
-              setShowStats(event.target.checked);
-            }}
-          />
-        </div>
+      )}
+    </div>
+  );
+}
+
+function MiscControlGroup(): React.JSX.Element {
+  const [showGround, setShowGround] = useAtom(atoms.showGround);
+  const [showStats, setShowStats] = useAtom(atoms.showStats);
+  const [autoRotate, setAutoRotate] = useAtom(atoms.autoRotate);
+
+  return (
+    <div className="ControlGroup">
+      <div className="ControlGroupTitle">Miscellaneous</div>
+      <div className="Control">
+        <label>Ground:</label>
+        <input
+          type="checkbox"
+          checked={showGround}
+          onChange={(event) => {
+            setShowGround(event.target.checked);
+          }}
+        />
+      </div>
+      <div className="Control">
+        <label>Auto rotate:</label>
+        <input
+          type="checkbox"
+          checked={autoRotate}
+          onChange={(event) => {
+            setAutoRotate(event.target.checked);
+          }}
+        />
+      </div>
+      <div className="Control">
+        <label>Stats:</label>
+        <input
+          type="checkbox"
+          checked={showStats}
+          onChange={(event) => {
+            setShowStats(event.target.checked);
+          }}
+        />
       </div>
     </div>
   );
