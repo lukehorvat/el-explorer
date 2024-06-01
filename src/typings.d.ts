1cfaccc67176e43b13edcf6cf102fa48bb723388
Luke Horvat <lukehorvat@gmail.com>
2024-06-01 18:01:05 +0200

    Add image typings

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,4 +1,19 @@
+declare module '*.png' {
+  const uri: string;
+  export default uri;
+}
+
 declare module '*.jpg' {
+  const uri: string;
+  export default uri;
+}
+
+declare module '*.gif' {
+  const uri: string;
+  export default uri;
+}
+
+declare module '*.svg' {
   const uri: string;
   export default uri;
 }
