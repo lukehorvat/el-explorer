61bccdf44addf3985c56f73d908fb39d82a241ec
Luke Horvat <lukehorvat@gmail.com>
2024-06-02 12:22:40 +0200

    Conditionally reset the animation type when changing actor

--- src/lib/state.ts
+++ src/lib/state.ts
@@ -1,7 +1,23 @@
 import { atom, createStore } from 'jotai';
+import { assetCache } from './asset-cache';
 
 const isLoaded = atom<boolean>(false);
 const actorType = atom<number>(77); // Initially default to Feros.
+const actorTypeWithEffects = atom(
+  (get) => get(actorType),
+  (get, set, newActorType: number) => {
+    set(actorType, newActorType);
+
+    // Reset the animation type if the new actor doesn't have the current one.
+    const actorDef = assetCache.actorDefs.get(newActorType)!;
+    const hasAnimation = actorDef.animations.find(
+      (animation) => animation.type === get(animationType)
+    );
+    if (!hasAnimation) {
+      set(animationType, null);
+    }
+  }
+);
 const skinType = atom<
   | 'texture'
   | 'wireframe'
@@ -26,7 +42,7 @@ const animationHandlers = atom<{
 
 export const stateAtoms = {
   isLoaded,
-  actorType,
+  actorType: actorTypeWithEffects,
   skinType,
   showSkeleton,
   showGround,
