3325d746616bd951d9a4df5f2deb36e9b3b352e5
Luke Horvat <lukehorvat@gmail.com>
2024-06-08 03:01:14 +0200

    Nav breadcrumbs

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { useAtom } from 'jotai';
+import { stateAtoms } from '../lib/state';
 import './TopBar.css';
 
 export function TopBar(): React.JSX.Element {
+  const [page, setPage] = useAtom(stateAtoms.page);
+
   return (
     <Navbar className="TopBar py-2">
       <Container className="px-3" fluid>
-        <Navbar.Brand>Eternal Lands Explorer</Navbar.Brand>
+        <Navbar.Brand href="#" onClick={() => setPage('home')}>
+          Eternal Lands Explorer
+        </Navbar.Brand>
+        {page !== 'loading' && page !== 'home' && (
+          <Navbar.Text className="me-auto fs-5">
+            <i className="bi-arrow-right me-3 fs-6" />
+            Actors
+          </Navbar.Text>
+        )}
         <Nav.Link
           href="https://github.com/lukehorvat/el-explorer"
           target="_blank"
