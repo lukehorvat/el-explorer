4992dcf695f8672e8127ee819b693827a18985b3
Luke Horvat <lukehorvat@gmail.com>
2024-06-09 13:11:49 +0200

    Generate json lists of files

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -1,10 +1,14 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+import path from 'node:path';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+import AddAssetPlugin from 'add-asset-webpack-plugin';
+import { globSync } from 'glob';
+
+const { dirname } = import.meta;
 
 /** @type { webpack.Configuration } */
-module.exports = {
+export default {
   devServer: {
     port: 9000,
     open: true,
@@ -14,11 +18,11 @@ module.exports = {
     },
   },
   entry: {
-    app: path.join(__dirname, 'src/index.tsx'),
-    'service-worker': path.join(__dirname, 'src/service-worker.ts'),
+    app: path.join(dirname, 'src/index.tsx'),
+    'service-worker': path.join(dirname, 'src/service-worker.ts'),
   },
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(dirname, 'dist'),
     filename: ({ runtime }) => {
       if (runtime === 'service-worker') {
         // Ensure that the service worker filename is stable (i.e. doesn't have a hash in it).
@@ -64,11 +68,29 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src/index.html'),
+      template: path.join(dirname, 'src/index.html'),
       excludeChunks: ['service-worker'],
     }),
     new CopyWebpackPlugin({
       patterns: [{ from: 'data', to: 'data' }],
     }),
+    new AddAssetPlugin(
+      'maps.json',
+      JSON.stringify(
+        globSync('**/*.elm.gz', { cwd: path.join(dirname, 'data/maps') })
+      )
+    ),
+    new AddAssetPlugin(
+      '3dobjects.json',
+      JSON.stringify(
+        globSync('**/*.e3d', { cwd: path.join(dirname, 'data/3dobjects') })
+      )
+    ),
+    new AddAssetPlugin(
+      '2dobjects.json',
+      JSON.stringify(
+        globSync('**/*.2d0', { cwd: path.join(dirname, 'data/2dobjects') })
+      )
+    ),
   ],
 };
